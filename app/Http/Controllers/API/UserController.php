b9e61152d5a903ee4d5b5ed71168d331697adcfd
ibrahimsamy308 <ibrahimsamy308@gmail.com>
2025-09-30 18:22:27 +0300

    update

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -32,7 +32,7 @@ class UserController extends Controller
 
 
     public function userProfile() {
-        $user =User::find(Auth::user()->id);
+        $user =User::with('courses')->find(Auth::user()->id);
         return new UserResource($user);        
     }
     
