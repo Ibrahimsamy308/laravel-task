0159f5bc88a7a33315f8d3637e6c96b948f92faa
ibrahimsamy308 <ibrahimsamy308@gmail.com>
2025-09-30 18:28:48 +0300

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -89,7 +89,7 @@ class User extends Authenticatable
 
     public function courses()
     {
-        return $this->hasMany(Course::class,'userCourses');
+        return $this->hasMany(Course::class,);
     }
     public function exams()
     {
